4dd0da587c8a5d3fe43caf15013500082d4210c3
sayalihandechavan <sayalichavan121090@gmail.com>
2026-05-22 00:36:54 +0530

    Search done

--- src/app/search/search.ts
+++ src/app/search/search.ts
@@ -1,11 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
 export class Search {
+  searchText = '';
+  results: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  onSearch() {
+    if(this.searchText) {
+      this.http.get(`https://jsonplaceholder.typicode.com/posts?q=${this.searchText}`)
+       .subscribe((data: any) => {
+          this.results = data;
+        });
+    }
+  }
 }
